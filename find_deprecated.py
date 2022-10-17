03c1d09a84bf09ed01d4376d6cada697a3c9f28e
Sarah Maris <sarah.maris@udacity.com>
2022-10-17 19:29:57 -0400

    Update comments and variable names

--- find_deprecated.py
+++ find_deprecated.py
@@ -7,83 +7,64 @@ import json
 import csv
 import re
 
-def create_issue_tracker_dict(data, nd, ver, lesson):
+def create_issue_tracker_dict(data):
     '''
     Create an issue tracker for the lesson
 
     INPUT
     data - holds the json loader to traverse the json
-    nd, ver, lesson  - values from the json or filename to create the URL
 
     RETURN
-    issue_tracker - dictionary with a key of the mocha URL and value of a
-                       list with 1 iterm being the concept/page name
+    issue_tracker - dictionary to hold results
     '''
     issue_tracker = {}
     count = 0
     for pages in data['concepts']:
         if count < len(data['concepts']):
-            page = pages['key'] # pages/concept key
-            url = '{}/{}/{}/{}'.format(nd, ver, lesson, page)
-            issue_tracker[url] = [data['concepts'][count]['title']] # add page title to list as values in dictionary
+            item_key = pages['key'] # pages/concept key
+            issue_tracker[item_key] = [data['concepts'][count]['title']] # add page title to list as values in dictionary
             count += 1 # increment the count to go to next page
     return issue_tracker
 
-# Atom Checks
-
-############### Chencai Programming Question Check ###############
-def programming_question_check(it, url, core):
+# Check for deprecated quizzes
+def deprecated_atom_check(it, item_key, core):
     if core['question']['semantic_type'] == "ProgrammingQuestion":
-          it[url].append(["Programming Question"])
+          it[item_key].append("Programming Question")
     elif core['question']['semantic_type'] == "ImageFormQuestion":
-          it[url].append(["Image Form Question"])
-############### Chencai Programming Question Check ###############
+          it[item_key].append("Image Form Question")
+    elif core['question']['semantic_type'] == "CodeGradedQuestion":
+          it[item_key].append("Code Graded Question")
+    elif core['question']['semantic_type'] == "QuestionInterface":
+        it[item_key].append("Question Interface")
+    elif core['question']['semantic_type'] == "IFrameQuestion":
+        it[item_key].append("IFrameQuestion")
 
-############### Chencai Workspace Check ###############
-def workspace_check(it, url, core):
-    if core['semantic_type'] == "WorkspaceAtom":
-          if core['pool_id'] == "jupyter":
-                it[url].append([core['pool_id'],core['name'],core['configuration']['blueprint']['conf']['defaultPath']])
-          elif core['pool_id'] == "sqlwidget":
-                it[url].append([core['pool_id'],core['name']," "])
-          else:
-                it[url].append([core['pool_id']," "," "])
-############### Chencai Workspace Check ###############
 
 # Customize Your Checks
-def initialize_checks(issue_tracker, nd, ver, lesson, data):
-    '''Does the initial check of the atom type and then will initiate the appropriate check.'''
-    it = issue_tracker
-    url = '{}/{}/{}/'.format(nd, ver, lesson)
+def initialize_checks(issue_tracker, cdkey, ver, lesson, data):
+    '''Checks atom type and initiates check for deprecated quiz types.'''
     for concept_num in range(0, len(data['concepts'])): # Every Concept (N)
-        url_key = url  + data['concepts'][concept_num]['key']
+        item_key = data['concepts'][concept_num]['key']
         for atom_num in range(0, len(data['concepts'][concept_num]['atoms'])): # Every Atom (M)
             core = data['concepts'][concept_num]['atoms'][atom_num]
             atom_type = core['__typename']
 
-############### Chencai Programming Question Check ###############
             if "QuizAtom" == atom_type:
-                programming_question_check(it, url_key, core)
-############### Chencai Programming Question Check ###############
+                deprecated_atom_check(issue_tracker, item_key, core)
+    return issue_tracker
 
-############### Chencai Workspace Check ###############
-            # if atom_type == "WorkspaceAtom":
-            #     workspace_check(it, url_key, core)
-############### Chencai Workspace Check ###############
-
-    return it
-
-# Finalize and Create the CSV for Output
-def clean_and_lesson_name(issue_tracker, lesson_name):
-    '''This will clean the dictionary to only pages with issues and if there are issues will add the lesson title'''
+# Create the CSV
+def clean_and_lesson_name(issue_tracker, cdkey, lesson_name):
+    '''This will clean the dictionary to include only pages with issues and if there are issues will add the lesson title'''
     for key in list(issue_tracker):
         if len(issue_tracker[key]) == 1:
             del issue_tracker[key]
         else:
             issue_tracker[key].insert(0, lesson_name)
+            issue_tracker[key].insert(0, cdkey)
     return issue_tracker
 
-def create_csv(issue_tracker, nd):
+def create_csv(issue_tracker):
     '''
     Creates a csv called 'issue_tracker.csv' from a dictionary 'issue_tracker'
 
@@ -91,43 +72,42 @@ def create_csv(issue_tracker, nd):
 
     OUTPUT - a CSV organized as follows
         Column Titles:
-         - Lesson Name - from JSON
-         - Page Name - from JSOn
-         - URL - goes to Mocha
-         - Issue - contain 1 issue in a given row
+         - Course Key (cdkey or part key)
+         - Lesson Name
+         - Page Name
+         - Issue
     '''
     with open('issue_tracker.csv', 'w') as csv_file:
         headers = ['Course Key', 'Lesson Name', 'Page Name', 'Issue'] # Labels
         writer = csv.DictWriter(csv_file, fieldnames = headers)
         writer.writeheader()
         for key,value in issue_tracker.items():
-            for items in range (2, len(value)):
-                writer.writerow({'Course Key': nd, 'Lesson Name': value[0], 'Page Name': value[1], 'Issue': value[items][0]})
+            writer.writerow({'Course Key': value[0], 'Lesson Name': value[1], 'Page Name': value[2], 'Issue': value[3]})
     print("Your course has been checked and your csv file is in the main directory.")
 
 def main():
     course_dict = {}
     for filename in os.listdir("jsons"):
-        #iterates through each json file
+
+        # iterates through each json file
         if filename.endswith('.json'):
-            # print(filename)
             file =  open("jsons/" + filename)
             data = json.load(file)
 
             # initialize static lesson values
             lesson = data['key'] #lesson key
-            nd = filename.split('_')[0] # nd key from filename
+            cdkey = filename.split('_')[0] # cdkey key from filename
             ver = filename.split('_')[2] # version from filename (this can't be found when exporting json from mocha/coco)
 
             # creates, checks, and organizes them into 1 dictionary
-            issue_tracker = create_issue_tracker_dict(data, nd, ver, lesson)
-            checked = initialize_checks(issue_tracker, nd, ver, lesson, data)
-            lesson_dict = clean_and_lesson_name(checked, data['title'])
+            issue_tracker = create_issue_tracker_dict(data)
+            checked = initialize_checks(issue_tracker, cdkey, ver, lesson, data)
+            lesson_dict = clean_and_lesson_name(checked, cdkey, data['title'])
             course_dict.update(lesson_dict)
             file.close()
 
     # uses the dictionary to create a csv
-    create_csv(course_dict, nd)
+    create_csv(course_dict)
 
 # Enter Values
 
